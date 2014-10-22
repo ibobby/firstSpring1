fa54b15d15e99fd134ddba700e007a71b3833aa9
ibobby <ibobby0386@gmail.com>
2014-10-22 21:58:22 +0400

    1

---
+++ src/main/java/beans/SpringFirstTest.java
@@ -0,0 +1,12 @@
+package beans;
+
+/**
+ * Created by WhiteMountiens on 19.10.2014.
+ */
+public class SpringFirstTest {
+
+    public void testMe() {
+        System.out.println("I am listening...");
+    }
+
+}
